e0ac0c763d440af9bbec26f8653744cd36375cf3
DmitriiPA <Dmitrii.PA@mail.ru>
2023-05-28 23:33:15 +0300

    fix: BEM and debag

--- src/index.js
+++ src/index.js
@@ -1,106 +1,107 @@
-// var tl = gsap.timeline();
-// tl.to(
-//   '.heder-stars-box img',  {
-//     opacity: 1, 
-//     delay: 0.7, 
-//     stagger: 0.04,
-//   }
-// )
-// .from(
-//   '.title',
-//   {
-//     y: -220,
-//     x: 400,
-//     duration: 0.8
-//   },
-//   1
-// )
-// .from(
-//   '.women', {y: -900},
-//   1.2
-// )
-// .from(
-//   '.field', {
-//     y: 400,
-//     opacity: 0,
-//   },
-//   1.2
-// )
-// .from(
-//   '.moon', {
-//     scale: 0,
-//     duration: 2,
-//    },
-//   1.5
-// )
-// .from(
-//   '.women', {
-//     rotate: 15,
-//     duration: 1.3,
-//     ease: "bounce.out",
-//   },
-//   1.7
-// )
-// .from(
-//   '.subtitle',
-//   {
-//     opacity: 0
-//   },
-//   1.8
-// )
-// .from(
-//   '.kye-box', {
-//     y: -600,
-//     duration: 1.4,
-//   },
-//   2
-// )
-// .from(
-//   '.kye-box', {
-//     rotate: 20,
-//     duration: 2.6,
-//     ease: Elastic.easeOut,
-//   },
-//   2
-// )
-// .from(
-//   '.nav_ul li', {
-//     x: -600,
-//     stagger: 0.1
-//   }, 
-//   '-=1'
-// )
+var tl = gsap.timeline();
+tl.to(
+  '.heder__stars__box img',  {
+    opacity: 1, 
+    delay: 0.7, 
+    stagger: 0.04,
+  }
+)
+.from(
+  '.title',
+  {
+    y: -220,
+    x: 400,
+    duration: 0.8
+  },
+  1
+)
+.from(
+  '.women', {y: -900},
+  1.2
+)
+.from(
+  '.field', {
+    y: 400,
+    opacity: 0,
+  },
+  1.2
+)
+.from(
+  '.moon', {
+    scale: 0,
+    duration: 2,
+   },
+  1.5
+)
+.from(
+  '.women', {
+    rotate: 15,
+    duration: 1.3,
+    ease: "bounce.out",
+  },
+  1.7
+)
+.from(
+  '.subtitle',
+  {
+    opacity: 0
+  },
+  1.8
+)
+.from(
+  '.kye__box', {
+    y: -600,
+    duration: 1.4,
+  },
+  2
+)
+.from(
+  '.kye__box', {
+    rotate: 20,
+    duration: 2.6,
+    ease: Elastic.easeOut,
+  },
+  2
+)
+.from(
+  '.nav__ul li', {
+    x: -600,
+    stagger: 0.1
+  }, 
+  '-=1'
+)
 
 
 const who = gsap.timeline()
 
-who.from ('.who-what-second-text', {
+who.from ('.who__what__second__text', {
   scrollTrigger: {
-    trigger: '.section-who-what',
+    // markers: true,
+    trigger: '.section__who__what',
     start: '+=30',
     end: '+=200',
     scrub: true,
   },
   y: 270,
 })
-.from ('.back-dlue', {
+.to ('.back__dlue', {
   scrollTrigger: {
-    trigger: '.section-who-what',
+    trigger: '.section__who__what',
     start: '+=50',
     end: '+=500',
     scrub: true,
   },
-  scale: 0
+  scale: 1500
 }
 )
-.from ('.back-yellow', {
+.to ('.back__yellow', {
   scrollTrigger: {
     // markers:true,
-    trigger: '.back-yellow',
+    trigger: '.back__yellow',
     start: '-=120 bottom',
     end: '+=200',
     scrub: true,
   },
-  scale: 0
+  scale: 1500
 }
 )
